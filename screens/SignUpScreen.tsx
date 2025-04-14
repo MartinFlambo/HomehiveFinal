cea5ed58c3548b7a35d9ec0e5fed53da26154dba
MartinFlambo <martinflambolopez@gmail.com>
2025-04-15 00:03:29 +0200

    Primer commit

---
+++ screens/SignUpScreen.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input, Button } from 'react-native-elements';
+import { StackScreenProps } from '@react-navigation/stack';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const auth = getAuth();
+
+const SignUpScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
+  const [value, setValue] = React.useState({
+    email: '',
+    password: '',
+    error: ''
+  })
+
+  async function signUp() {
+    if (value.email === '' || value.password === '') {
+      setValue({
+        ...value,
+        error: 'Email and password are mandatory.'
+      })
+      return;
+    }
+  
+    try {
+      await createUserWithEmailAndPassword(auth, value.email, value.password);
+      navigation.navigate('Sign In');
+    } catch (error) {
+      if (error instanceof Error) {
+        setValue({
+          ...value,
+          error: error.message,
+        })
+        return;
+      }
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Signup screen!</Text>
+
+      {!!value.error && <View style={styles.error}><Text>{value.error}</Text></View>}
+
+      <View style={styles.control}>
+        <TextInput
+          placeholder='Email'
+          style={styles.TextInput}
+          value={value.email}
+          onChangeText={(text) => setValue({ ...value, email: text })}
+        />
+
+        <TextInput
+          placeholder='Password'
+          style={styles.TextInput}
+          value={value.password}
+          onChangeText={(text) => setValue({ ...value, password: text })}
+          secureTextEntry={true}
+        />
+
+        <Button title="Sign up" buttonStyle={styles.control} onPress={signUp} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  TextInput:{
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  control: {
+    marginTop: 10
+  },
+
+  error: {
+    marginTop: 10,
+    padding: 10,
+    color: '#fff',
+    backgroundColor: '#D54826FF',
+  }
+});
+
+export default SignUpScreen;
