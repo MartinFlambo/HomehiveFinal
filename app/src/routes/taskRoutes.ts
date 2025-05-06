cee5c9aff56ae157c40881006f0144612f52bb13
MartinFlambo <martinflambolopez@gmail.com>
2025-05-06 19:52:15 +0200

    get method

--- app/src/routes/taskRoutes.ts
+++ app/src/routes/taskRoutes.ts
@@ -6,9 +6,7 @@ import protectRoute from "../middleware/auth.middleware";
 const router = express.Router();
 
 // Lista de imágenes válidas
-const allowedImages = [
-  "aspirar.jpg",
-];
+const allowedImages = ["aspirar.jpg"];
 
 router.post("/", protectRoute, async (req, res) => {
   try {
@@ -22,7 +20,7 @@ router.post("/", protectRoute, async (req, res) => {
       title,
       description,
       dificult,
-      image, 
+      image,
       user: req.user!._id,
     });
 
@@ -30,6 +28,27 @@ router.post("/", protectRoute, async (req, res) => {
   } catch (err) {
     console.error("Error al crear tarea:", err);
     res.status(500).json({ message: "Error al crear la tarea" });
+  }
+});
+
+router.get("/", protectRoute, async (req, res) => {
+  try {
+    const page = Number(req.query.page as string) || 1;
+    const limit = Number(req.query.limit as string) || 5;
+    const skip = (page - 1) * limit;
+
+    const tasks = await Task.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage");
+
+    const totalTasks = await Task.countDocuments();
+
+    res.send({tasks, currentPage: page, totalTasks: totalTasks, totalPages: Math.ceil(totalTasks/limit)});
+  } catch (error) {
+    console.log("Error al obtener las tareas:", error);
+    res.status(500).json({ message: "Error del servidor" });
   }
 });
 
