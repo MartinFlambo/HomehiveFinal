47ab958c998cf12a11249159ba41ff32f923b30a
Martín Flambó López <129385746+MartinFlambo@users.noreply.github.com>
2025-05-14 17:38:49 +0200

    Cron job and change url from local to render.com (#20)

---
+++ app/src/lib/cron.ts
@@ -0,0 +1,18 @@
+import cron from "cron";
+import https from "https";
+
+const job = new cron.CronJob("*/14 * * * *", function(){
+    const url = process.env.API_URL;
+    if(!url){
+        console.error("API_URL is not defined");
+        return;
+    }
+    https.get(url, (res)=>{
+        if(res.statusCode === 200)
+            console.log ("Cron job executed successfully");
+        else
+            console.log ("Error executing cron job", res.statusCode);
+    }).on("error", (err)=> console.error("Error while sending request", err));
+})
+
+export default job
