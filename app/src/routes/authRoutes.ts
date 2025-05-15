a4d0618aad6ecc16d22b7e3cffab6842faff1751
MartinFlambo <martinflambolopez@gmail.com>
2025-05-15 14:51:42 +0200

    Fix get from database to login

--- app/src/routes/authRoutes.ts
+++ app/src/routes/authRoutes.ts
@@ -82,7 +82,7 @@ router.post("/login", async (req: Request, res: Response): Promise<Response> => 
         return res.status(400).json({ message: "Debes rellenar todos los campos" });
       }
   
-      const user = await User.findOne({ email })
+      const user = await User.findOne({ email }).select("+password");
       if (!user) {
         return res.status(400).json({ message: "Introduce las credenciales correctas" });
       }
