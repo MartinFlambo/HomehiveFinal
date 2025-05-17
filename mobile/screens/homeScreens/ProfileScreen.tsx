d4bb0c1896947090d69951fd243b4503be3bdf51
MartinFlambo <martinflambolopez@gmail.com>
2025-05-17 18:50:17 +0200

    fix profilepic format

--- mobile/screens/homeScreens/ProfileScreen.tsx
+++ mobile/screens/homeScreens/ProfileScreen.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-elements';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Image } from "react-native-elements";
 import { useAuthStore } from "../../store/authStore";
 
-
 export default function CreateTaskScreen() {
+  const [task, setTasks] = useState([]);
+  const { user } = useAuthStore();
+  console.log(user)
   return (
     <View style={styles.container}>
-      <Text>Perfil</Text>
+      <Image
+        source={{ uri: user?.profileImage }}
+        style={{ width: 100, height: 100, borderRadius: 50 }}
+      />
     </View>
   );
 }
@@ -15,11 +20,11 @@ export default function CreateTaskScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   button: {
-    marginTop: 10
-  }
+    marginTop: 10,
+  },
 });
